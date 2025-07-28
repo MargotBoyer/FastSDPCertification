55a5f99e7310b8e8b4d1885bbeffdac6f69b4d36
MargotBoyer <margot.boyerblr@gmail.com>
2025-07-28 15:05:25 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="certification_problem",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
+
+
+
+
